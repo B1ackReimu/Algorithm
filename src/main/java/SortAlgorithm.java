2497de18baf69ede24734c965ac52b0364a583cf
BlackReimu <BlackReimu@2333.com>
2021-08-09 08:55:50 +0800

    int[100]

--- src/main/java/SortAlgorithm.java
+++ src/main/java/SortAlgorithm.java
@@ -3,29 +3,49 @@ import java.util.Random;
 
 public class SortAlgorithm {
 
-    static int[] ints = new int[100];
+    static int[] ints = new int[20];
 
     public static void main(String[] args) {
         Random random = new Random();
         for (int i = 0; i < ints.length; i++) {
-            int a = random.nextInt(100000);
+            int a = random.nextInt(1000);
             ints[i] = a;
         }
-        bubble();
+        bubble(true);
+        System.out.println(Arrays.toString(ints));
+        bubble(false);
         System.out.println(Arrays.toString(ints));
     }
 
-    static void bubble() {
+    static void bubble(boolean reverse) {
         int length = ints.length;
         for (int i = 0; i < length; i++) {
-            for (int j = length-1; j > 0; j--) {
-                int tmp;
-                if (ints[j-1]<=ints[j]){
-                    tmp=ints[j];
-                    ints[j]=ints[j-1];
-                    ints[j-1]=tmp;
+            boolean out = false;
+
+            int tmp;
+            if (reverse) {
+                for (int j = length - 1; j > 0; j--) {
+                    if (ints[j - 1] < ints[j]) {
+                        out = true;
+                        tmp = ints[j];
+                        ints[j] = ints[j - 1];
+                        ints[j - 1] = tmp;
+                    }
+                }
+            } else {
+                for (int j = 0; j < length - 1; j++) {
+                    if (ints[j] > ints[j + 1]) {
+                        out = true;
+                        tmp = ints[j];
+                        ints[j] = ints[j + 1];
+                        ints[j + 1] = tmp;
+                    }
                 }
             }
+            if (!out) {
+                break;
+            }
+            System.out.println(i);
         }
     }
 
